571974ab108899feb4b47b0ab2385c0a8e196a85
Cori <corinne.a.ader@gmail.com>
2019-08-16 11:08:47 -0400

    templates for all pages and began courses page

---
+++ public/scripts/courses.js
@@ -0,0 +1,37 @@
+"use strict";
+
+//document ready event handler
+$(function() {
+    let ddObjs;
+
+	//retrieving category data from json file
+	$.getJSON("/api/categories", function(data) {
+		ddObjs = data;
+		for (let i = 0; i < ddObjs.length; i++) {
+			$("#catSelect").append('<option value="' + ddObjs[i].Value + '">' + ddObjs[i].Category + "</option>");
+		}
+	});
+
+    $("#catSelect").on("change", function() {
+	$.getJSON(("/api/courses/bycategory/" + $("#catSelect").val()), function(data) {
+
+        let coursesObjs = data;
+        $("#tableBody").empty();
+		let objLen = coursesObjs.length;
+		for (let i = 0; i < objLen; i++) {
+			let dynamicTableRow =
+				"<tr><td>" +
+				coursesObjs[i].CourseId +
+				"</td><td>" +
+				coursesObjs[i].Title +
+				"</td><td>" +
+				coursesObjs[i].StartDate +
+				"</td><td><a href='details.html?id=" +
+				coursesObjs[i].CourseId +
+				"'>View Details</a></td></tr>";
+            $("#tableBody").append(dynamicTableRow);
+            $("#courseTblOutput").show();
+		}
+    });
+});
+})
