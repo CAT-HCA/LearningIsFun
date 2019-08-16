5ee617ec70937efccfe381ffba176faea57785e8
Cori <corinne.a.ader@gmail.com>
2019-08-16 17:46:14 -0400

    functionality complete, pending styling updates and validation

--- public/scripts/details.js
+++ public/scripts/details.js
@@ -12,34 +12,36 @@ $(function() {
 		buildRow("Start Date", data.StartDate);
 		buildRow("End Date", data.EndDate);
 		buildRow("Meets", data.Meets);
-        buildRow("Fee", ("$" + data.Fee));
-        buildRow("Students Enrolled", data.Students.length);
+		buildRow("Fee", "$" + data.Fee);
+		buildRow("Students Enrolled", data.Students.length);
 
-        let regBtnTag = '<div class="text-center"><a class="btn btn-primary w-25 text-center" href="register.html?id=' + courseId + '" id="registerBtn">REGISTER FOR THIS COURSE</a></div>'
-        $("#detailTblOutputForm").append(regBtnTag);
+		let regBtnTag =
+			'<div class="text-center"><a class="btn btn-primary w-25 text-center" href="register.html?id=' +
+			courseId +
+			'" id="registerBtn">REGISTER FOR THIS COURSE</a></div>';
+		$("#detailTblOutputForm").append(regBtnTag);
 
-        if(data.Students.length > 0){
-            $("#studentTblOutput").show();
-            for(let i = 0; i < data.Students.length; i++){
-            buildRow(data.Students[i].studentname, data.Students[i].email);
-        }
-        }
-        else {
-            $("#noStudentsDiv").show();
-        }
-    });
-
-
-
+		if (data.Students.length > 0) {
+			$("#studentTblOutput").show();
+			for (let i = 0; i < data.Students.length; i++) {
+				buildStudentRow(data.Students[i].StudentName, data.Students[i].Email);
+			}
+		} else {
+			$("#noStudentsDiv").show();
+		}
+	});
 });
 
 function buildRow(property, value) {
 	let propString = property.split(" ");
-    let inputId = propString.join("").toLowerCase() + "OutputField";
-    let markup = 
-    '<tr><td>' + property + '</td><td id="' + inputId + '">' + value + '</td></tr>'
-    $("#tableBody").append(markup);
-} 
+	let inputId = propString.join("").toLowerCase() + "OutputField";
+	let markup = "<tr><td>" + property + '</td><td id="' + inputId + '">' + value + "</td></tr>";
+	$("#tableBody").append(markup);
+}
 
-
-
+function buildStudentRow(property, value) {
+	let propString = property.split(" ");
+	let studentInputId = propString.join("").toLowerCase() + "OutputField";
+	let studentMarkup = "<tr><td>" + property + '</td><td id="' + studentInputId + '">' + value + "</td></tr>";
+	$("#studentTableBody").append(studentMarkup);
+}
